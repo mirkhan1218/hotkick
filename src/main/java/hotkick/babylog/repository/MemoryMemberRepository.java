2c7450a906787236af178836c1afdb139902682f
mirkhan <mirkhan1218@gmail.com>
2023-06-15 12:09:55 +0900

    Bean 등록 - Annotation 이용

--- src/main/java/hotkick/babylog/repository/MemoryMemberRepository.java
+++ src/main/java/hotkick/babylog/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hotkick.babylog.repository;
 
 import hotkick.babylog.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
